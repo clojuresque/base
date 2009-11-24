b186dcc1efbb8c487db5b256900079e1cccd1078
Meikel Brandmeyer <mb@kotka.de>
2009-11-24 21:06:17 +0100

    Bubble out *all* Exceptions out of ClojureCompile
    Fixes #1

--- src/main/java/clojuresque/ClojureCompile.java
+++ src/main/java/clojuresque/ClojureCompile.java
@@ -95,8 +95,6 @@ public class ClojureCompile {
                     rdr.close();
                 }
             }
-        } catch(IOException e) {
-            e.printStackTrace();
         } finally {
             Var.popThreadBindings();
         }
